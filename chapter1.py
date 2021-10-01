33a4793371ee11c872278a5978b63fa180d45130
sisko <adeoduye@gmail.com>
2021-10-01 03:33:48 +0100

    Successfully edge-detects lena image

--- chapter1.py
+++ chapter1.py
@@ -4,7 +4,9 @@ img = cv2.imread("Resources/lena.png")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
+imgCanny = cv2.Canny(img, 100, 100)
 
 cv2.imshow("ShowGrey", imgGray)
 cv2.imshow("ShowBlur", imgBlur)
+cv2.imshow("ShowEdges", imgCanny)
 cv2.waitKey(0)
