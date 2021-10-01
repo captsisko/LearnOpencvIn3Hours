425bc919b414664f39af8db4efdf9fbdf84f7dee
sisko <adeoduye@gmail.com>
2021-10-01 04:05:06 +0100

    Successfully crops lambo image

--- chapter1.py
+++ chapter1.py
@@ -6,7 +6,10 @@ print(img.shape)
 imgResize = cv2.resize(img, (250, 150))
 print(imgResize.shape)
 
+imgCropped = img[0:200, 200:500]
+
 cv2.imshow("imgShow", img)
 cv2.imshow("imgResized", imgResize)
+cv2.imshow("imgCropped", imgCropped)
 
 cv2.waitKey(0)
