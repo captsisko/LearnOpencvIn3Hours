3b111d7be4d31707e6fdd4a691bdd0d078b9c181
sisko <adeoduye@gmail.com>
2021-10-01 03:26:16 +0100

    Successfully greys lena image

--- chapter1.py
+++ chapter1.py
@@ -1,12 +1,8 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-cap.set(10, 100)
+img = cv2.imread("Resources/lena.png")
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("ShowGrey", imgGray)
+cv2.waitKey(0)
