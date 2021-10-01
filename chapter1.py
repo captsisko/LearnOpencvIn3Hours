57ab9a2f610f8cb0e5ed355472fd848f23ed2545
sisko <adeoduye@gmail.com>
2021-10-01 03:30:37 +0100

    Successfully blurs lena image

--- chapter1.py
+++ chapter1.py
@@ -3,6 +3,8 @@ import cv2
 img = cv2.imread("Resources/lena.png")
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
 
 cv2.imshow("ShowGrey", imgGray)
+cv2.imshow("ShowBlur", imgBlur)
 cv2.waitKey(0)
